7ca9706cc9d94bbd7f873c454b1bafc5ef4d40a6
Manerova Olga <o.manerova@qsoft.ru>
2021-11-30 18:04:11 +0300

    правки код стайла

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
 'use strict'
 
-let title = 'JavaScript Курс';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 100;
+
 let rollback = 10;
 let fullPrice = 15000;
-let adaptive = true;
+let service1, service2, servicePrice1, servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback/100));
+
+// let title = prompt('Как называется ваш проект?');
+// let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
+// let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+// service1 = prompt('Какой дополнительный тип услуги нужен?');
+servicePrice1 = +prompt('Сколько это будет стоить?');
+// service2 = prompt('Какой дополнительный тип услуги нужен?');
+servicePrice2 = +prompt('Сколько это будет стоить?');
+
+// fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+
+// const showTypeOf = function(variable) {
+//     console.log(variable, typeof variable)
+// }
+
+// const getRollbackMessage = function(price) {
+//     if (price > 30000) {
+//         return 'Даем скидку в 10%';
+//     } else if (price > 15000 && price <= 30000) {
+//         return 'Даем скидку в 5%';
+//     } else if (price <= 15000 && price >= 0) {
+//         return 'Скидка не предусмотрена';
+//     } else if (price < 0) {
+//         return 'Что-то пошло не так';
+//     }
+// }
+
+const allServicePrices = function getAllServicePrices() {
+    return servicePrice1 + servicePrice2;
+}
+
+fullPrice = function getFullPrice() {
+    return screenPrice + allServicePrices();
+}
+
+console.log(allServicePrices());
+console.log(fullPrice());
+
+// showTypeOf(title);
+// showTypeOf(fullPrice);
+// showTypeOf(adaptive);
+
 
 // alert('Первый урок');
 // console.log('Первый урок');
 
-// console.log(typeof title);
-// console.log(typeof fullPrice);
-// console.log(typeof adaptive);
 
 // console.log(screens.length);
 
@@ -23,40 +64,5 @@ let adaptive = true;
 
 // console.log(fullPrice * (rollback/100));
 
-// №3
-title = prompt('Как называется ваш проект?');
-
-// №4
-screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
-// №5
-screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
-
-// №6
-adaptive = confirm('Нужен ли адаптив на сайте?');
-
-// №7
-let service1, service2, servicePrice1, servicePrice2;
-
-service1 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice1 = +prompt('Сколько это будет стоить?');
-service2 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice2 = +prompt('Сколько это будет стоить?');
-
-// №8
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-// №9
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback/100));
-console.log(servicePercentPrice);
-
-// №10
-if (fullPrice > 30000) {
-    alert('Даем скидку в 10%');
-} else if (fullPrice > 15000 && fullPrice <= 30000) {
-    alert('Даем скидку в 5%');
-} else if (fullPrice <= 15000 && fullPrice >= 0) {
-    alert('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
-    alert('Что-то пошло не так');
-}
+// console.log(servicePercentPrice);
+// console.log(getRollbackMessage(fullPrice));
